87d2b6176f30376198aa1ee6f562b7286c9c6be0
Ashley Hunt <ashhunt07@gmail.com>
2020-09-27 00:00:58 -0500

    adding in comments to help developers follow code

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
+// Write code to use inquirer to gather information about the development team members, and to create objects for each team member 
+
+//Team information and details
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+//requirements
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+//output results
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+//Pulling into from htmlRender file for styling and layout
 const render = require("./lib/htmlRenderer");
 
+
+//Empty array for team members to be added
 const team = [];
 
 
+//Initialize function and start add members to your team
 async function init() {
     promptManager();
 }
+
+
 // Questions for Manager & create new Manager
 const promptManager = () => {
     inquirer
@@ -42,6 +53,7 @@ const promptManager = () => {
             },
         ])
 
+    // Adds manager to team array
     .then(function(input) {
         console.log("manager");
         const manager = new Manager(input.manager, input.managerId, input.managerEmail, input.managerOffice)
@@ -52,7 +64,7 @@ const promptManager = () => {
 }
 
 
-//Create a list of Engineers & Interns
+//Run to create new Engineers and interns to add to the team.html file
 function buildTeam() {
     inquirer.prompt([{
         type: "list",
@@ -110,6 +122,7 @@ const promptEngineer = () => {
             },
         ])
 
+    // Adds engineer to team array
     .then(function(input) {
         console.log("engineer");
         const engineer = new Engineer(input.engineer, input.engineerId, input.engineerEmail, input.engineerGithub)
@@ -145,6 +158,7 @@ const promptIntern = () => {
             },
         ])
 
+    // Adds intern to team array
     .then(function(input) {
         console.log("intern");
         const intern = new Intern(input.intern, input.internId, input.internEmail, input.internSchool)
@@ -166,32 +180,5 @@ const assembleTeam = async() => {
 };
 
 
-
-
-// function call to initialize program
+// function call to initialize entire program
 init();
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
